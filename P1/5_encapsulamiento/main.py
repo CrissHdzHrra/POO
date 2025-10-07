e371496a0d426b9ae2905bd9611ba48fc9f26e8e
CrisspunK <121061203+CrissHdzHrra@users.noreply.github.com>
2025-10-06 19:09:47 -0600

    main.py

---
+++ P1/5_encapsulamiento/main.py
@@ -0,0 +1,20 @@
+
+from coches import Coches
+num_coches=int(input("¿Cuantos coches deseas?: "))
+
+for i in range(0,num_coches):
+    print(f"\n\t...Datos del Coche...{i+1}")
+    marca=input(" marca del coche: ").upper()
+    color=input(" color del coche: ").upper()
+    modelo=input(" modelo del coche: ").upper()
+    velocidad=int(input("maxima del coche: "))
+    potencia=int(input("caballaje del coche: "))
+    plazas=int(input(" numero de plazas del coche: "))
+
+    #Crea un objeto o instancia de la clase Coches
+
+    coche1=Coches(marca,color,modelo,velocidad,potencia,plazas)
+
+    print(f"Datos del Vehiculo \n Marca: {coche1.getMarca()} \n Color: {coche1.getColor()} \n Modelo: {coche1.getModelo()} \n Velocidad Maxima: {coche1.getVelocidad()} \n Caballaje: {coche1.getCaballaje()} \n Numero de Plazas: {coche1.getPlazas()}")
+
+    print(f"\n\n\t {coche1.acelerar()}")
