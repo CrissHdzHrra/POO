ee76f2fdb6af252a9cd7a71b1612f0faae3ce6c6
CrisspunK <121061203+CrissHdzHrra@users.noreply.github.com>
2025-12-05 08:35:57 -0600

    commit_05_12_25

--- P3/PROYECTOS_TKINTER/coches_system/conexionBD.py
+++ P3/PROYECTOS_TKINTER/coches_system/conexionBD.py
@@ -1,1 +1,17 @@
-#commit_01_12_25
+#commit_05_12_25
+
+import mysql.connector
+
+try:
+    #Conectar con la BD en MySQL
+    conexion=mysql.connector.connect(
+        host='localhost',
+        user='root',
+        port=3307,
+        password='',
+        database='coches_system'
+    )
+    #Crear un objeto de tipo cursor que se pueda reutilizar nuevamente
+    cursor=conexion.cursor(buffered=True)
+except:
+     print(f"Ocurrio un error con el Sistema por favor verifique ...")    
